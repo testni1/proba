7d95fb9563433e77e97434adc06b9bae68240a88
Njegos Rajcevic <njegosr406@gmail.com>
2019-07-08 13:02:51 +0200

    Zvjezdice

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ for(let i=0; i<10; i++) {
     console.log('Hello World...' + i);
 }
 console.log('Ja ne znam sta se desava!!!');
+for(let i = 1; i < 15; i++) {
+    for(let j=0; j< i; j++) {
+        console.log('*');
+    }
+}
